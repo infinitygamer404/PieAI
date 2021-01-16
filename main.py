09819ca46bfc9cdebd13ee694408d56aa206b506
jpie726 <jonah.pierce.00@gmail.com>
2021-01-15 19:46:04 -0600

    Pub for dbg help

--- main.py
+++ main.py
@@ -2,27 +2,20 @@ import discord
 from discord.ext import commands
 import new_com
 
-TOKEN = open('token.txt').read()
-print(TOKEN)
 
-def main(TOKEN):
+def main():
     client = commands.Bot(command_prefix='.pie ')
-
-    @client.command(name='NAME')
-    async def _NAME(ctx):
-        embed = discord.Embed(title=f'{client.user.name} - [DESCRIPTION]', description=f'[INFO]')
-        await ctx.send(embed=embed)
-
-    @new_com.new()
-    def NewCommand():
-        pass
 
     @client.event
     async def on_ready():
         print(f'Login as {client.user.name}')
-        # NOW SET THE ACTIVITY
-        await client.change_presence(activity=discord.Game(name=f'{client.command_prefix}ping'))
+        await client.change_presence(activity=discord.Game(name=f'{client.command_prefix}'))
 
-    client.run(TOKEN)
+    @new_com.new(description='test_description', name='test_name', prefix='.pie ', title='test_title')
+    async def NewCommand():
+        pass
 
-main(TOKEN)
+    client.run(open('token.txt').read())
+
+
+main(open('token.txt').read())
