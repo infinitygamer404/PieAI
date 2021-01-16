124b1f63d819df61848c8a73ca0d63037dc1f09d
jpie726 <jonah.pierce.00@gmail.com>
2021-01-16 17:46:35 -0600

    commit

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from discord.ext import commands
 
 
 def main():
-    client = commands.Bot(command_prefix='.pie ')
+    '''
+        \@client.command(name='')
+        async def ping(ctx):
+            await ctx.send(embed=discord.Embed(title='', description='', footer=''))
+    '''
+
+    client = commands.Bot(command_prefix='.p ')
+
+    @client.command(name='new')
+    async def new(ctx):
+        await ctx.send(embed=discord.Embed(title='', description='', footer=''))
+
+    @client.command(name='ping')
+    async def ping(ctx):
+        await ctx.send(embed=discord.Embed(title='Pong', description=f'{int(client.latency * 1000)}ms', footer='')), print(f'Pinged by {discord.User.id}!')
 
     @client.event
-    async def on_ready():
-        print(f'Login as {client.user.name}')
-        await client.change_presence(activity=discord.Game(name=f'{client.command_prefix}'))
-
-    @client.command(name='test2_name')
-    async def _ping(ctx):
-        await ctx.send(embed=discord.Embed(title='title', description='description', footer=''))
+    async def on_ready():print(f'Login as {client.user.name}');await client.change_presence(activity=discord.Game(name=f'{client.command_prefix}'));
 
     client.run(open('token.txt').read())
 
 
+def help():
+    client = commands.Bot(command_prefix='.p ')
+
+
 if __name__ == '__main__':
     main()
+    help()
