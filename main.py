c2043cd115ed9f5d72d8bd96accab3dc323a3f99
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2021-01-17 10:26:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,47 @@ def main():
             await ctx.send(embed=discord.Embed(title='', description='', footer=''))
     '''
 
-    help = commands.Bot(command_prefix=f'')
     client = commands.Bot(command_prefix='.p ')
 
-    x = discord.Member.mentioned_in()
-    print(x)
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
+        game = discord.Game(name=f'{client.command_prefix}')
+        await client.change_presence(activity=game)
+
 
     @client.command(name='new')
     async def new(ctx):
         await ctx.send(embed=discord.Embed(title='', description='', footer=''))
 
+
     @client.command(name='ping')
     async def ping(ctx):
-        await ctx.send(embed=discord.Embed(title='Pong', description=f'{int(client.latency * 1000)}ms', footer='')), print(f'Pinged by {ctx.author.name}#{ctx.author.discriminator}, {ctx.author.id}!')
+        e = discord.Embed(title='Pong',
+        description=f'{int(client.latency * 1000)}ms',
+        footer='')
+
+        await ctx.send(embed=e)
+        print(f'Pinged by {ctx.author}!')
+
+
+    @client.event
+    async def on_message(message):
+        if client.user.mentioned_in(message):
+            e = discord.Embed(title='Help',
+            description='Use .p to signal you want to use a command',
+            footer='Use .p help for help')
+
+            await message.channel.send(embed=e)
+        else:
+            pass
+
+        await client.process_commands(message)
+
 
     client.run(open('token.txt').read())
 
 
 if __name__ == '__main__':
     main()
-    help()
+
